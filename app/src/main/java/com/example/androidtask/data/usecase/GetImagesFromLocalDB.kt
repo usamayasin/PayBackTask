fecefc908580c165c957aeb6c46b3d7a91770f75
Usama Yasin <uyasin@pie-int.com>
2022-03-09 01:15:00 +0500

    Turbine added Instrumental tests for local repo

--- app/src/main/java/com/example/androidtask/data/usecase/GetImagesFromLocalDB.kt
+++ app/src/main/java/com/example/androidtask/data/usecase/GetImagesFromLocalDB.kt
@@ -1,9 +1,7 @@
 package com.example.androidtask.data.usecase
 
-import com.example.androidtask.data.local.entity.ImagesEntity
 import com.example.androidtask.data.local.repository.LocalRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetImagesFromLocalDB @Inject constructor(
--- app/src/test/java/com/example/androidtask/usecase/FetchImagesUsecaseTest.kt
+++ app/src/test/java/com/example/androidtask/usecase/FetchImagesUsecaseTest.kt
@@ -1,12 +1,8 @@
 package com.example.androidtask.usecase
 
-import android.util.Log
+import app.cash.turbine.test
 import com.example.androidtask.MockTestUtil
-import com.example.androidtask.data.local.entity.ImagesEntity
-import com.example.androidtask.data.local.entity.toDomainModel
 import com.example.androidtask.data.local.repository.AbstractLocalRepo
-import com.example.androidtask.data.local.repository.LocalRepository
-import com.example.androidtask.data.model.Image
 import com.example.androidtask.data.model.ImageList
 import com.example.androidtask.data.remote.DataState
 import com.example.androidtask.data.repository.Repository
@@ -15,12 +11,10 @@ import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -57,34 +51,5 @@ class FetchImagesUsecaseTest {
             Assert.assertEquals(givenImages.error, it)
         }
 
-
-
-
-
     }
-/*
-    @Test
-    fun `test invoking FetchimageInfoimagecase gives imageList`() = runBlocking {
-        // Given
-        val fetchImagesInfoUseCase = FetchImagesUseCase(repository, localRepo)
-        val givenImages = MockTestUtil.getMockImages(1)
-
-        // When
-        coEvery { repository.getImages(page = any() , keyword = any()) }
-            .returns(flowOf(DataState.Error<ImageList>(DataState.CustomMessages.BadRequest)))
-
-        fetchImagesInfoUseCase.invoke(9,"rice"){
-
-        }
-
-        localRepo.getImages.collectLatest {
-          Assert.assertEquals(givenImages.hits, it)
-        }
-
-    }*/
-
-
-
-
-
 }
